f3f387edc838e625ac73af231ebc3771ac259230
Krishna Srinivas <krishna.srinivas@gmail.com>
2016-08-10 01:02:02 +0530

    Add comments

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/vulcand/oxy/forward"
 )
 
+// Trap ServeHTTP's ResponseWriter so that response headers and body can be
+// written to Stdout.
 type responseWriterTrap struct {
 	writer http.ResponseWriter
 }
@@ -33,13 +35,14 @@ func (w responseWriterTrap) WriteHeader(i int) {
 	w.writer.WriteHeader(i)
 }
 
+// To log the request headers and body to Stdout.
 type logger struct {
 	h http.Handler
 }
 
 func (l logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("\n---------------------------\n")
-	fmt.Printf("REQUEST : %s %s\n", r.Method, r.URL)
+	fmt.Printf("REQUEST : %s %s?%s\n", r.Method, r.URL.Path, r.URL.RawQuery)
 	fmt.Printf("Host: %s\n", r.Host)
 	for k, v := range r.Header {
 		fmt.Printf("%s: %s\n", k, v[0])
@@ -49,16 +52,11 @@ func (l logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("\n--------------------------\n")
 }
 
+// To forward the request to the address specified with -f
 type forwarder struct {
 	scheme string
 	host   string
 	h      http.Handler
-}
-
-type closer func() error
-
-func (c closer) Close() error {
-	return c()
 }
 
 func (f forwarder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -77,14 +75,23 @@ func (f forwarder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	f.h.ServeHTTP(w, r)
 }
 
+// To typecast a func to io.Closer
+type closer func() error
+
+func (c closer) Close() error {
+	return c()
+}
+
 func main() {
 	listenAddr := flag.String("l", ":8000", "listen address")
 	fwdAddr := flag.String("f", "localhost:9000", "forward address")
 	flag.Parse()
+
 	fwd, _ := forward.New(forward.PassHostHeader(true))
 	server := &http.Server{
 		Addr:    *listenAddr,
 		Handler: logger{forwarder{"http", *fwdAddr, fwd}},
 	}
-	server.ListenAndServe()
+
+	fmt.Println(server.ListenAndServe())
 }
